0cc7ac8a267886b04c77536bfba59f19d73139a0
Ken Masters <mskenmasters@hotmail.com>
2023-08-14 15:42:26 +0200

    Añade los componentes ahorcado.tsx y palabrasproporcionadas.tsx con la y modificar el main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Ahorcado } from './Ahorcado';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		{/* <App /> */}
-	</React.StrictMode>
+import 'bootstrap/dist/css/bootstrap.css';
+import { PalabrasProporcionadas } from './PalabrasProporcionadas';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+	      <PalabrasProporcionadas>
+        {(palabraProporcionada) => <Ahorcado palabraProporcionada={palabraProporcionada} />}
+      </PalabrasProporcionadas>
+  </React.StrictMode>
 );
