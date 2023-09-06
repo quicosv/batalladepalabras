1d0ae137e9e4ef80e08dc555de6e0ac5393afd5c
piscinagua <correo_javi@hotmail.com>
2023-09-06 17:34:31 +0200

    Modifica modelos, controladores y demás, tanto de palabras como de los jugadores.

--- backend/models/palabra.ts
+++ backend/models/palabra.ts
@@ -2,15 +2,21 @@ import { DataTypes, Model } from 'sequelize';
 import { db } from '../database/config';
 
 interface PalabraAttributes {
+	idpalabra?: number;
 	palabra: string;
 }
 
 export const Palabra = db.define<Model<PalabraAttributes>>(
 	'Palabra',
 	{
-		palabra: {
-			type: DataTypes.STRING(25),
+		idpalabra: {
+			allowNull: false,
 			primaryKey: true,
+			autoIncrement: true,
+			type: DataTypes.INTEGER.UNSIGNED,
+		},
+		palabra: {
+			type: DataTypes.STRING(30),
 			allowNull: false
 		}
 	},
