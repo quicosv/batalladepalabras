c7e3a132037fb8b7dea303ff9aa97a8808932827
Quico Saval <quicosv@gmail.com>
2023-09-06 18:39:55 +0200

    Añade variables para crear títulos y encabezados

--- frontend/src/variables.ts
+++ frontend/src/variables.ts
@@ -1,1 +1,7 @@
 const aplicacion = 'Batalla de palabras';
+const h1Login = 'Inicio de sesión';
+const h1Registro = 'Creación de cuenta';
+const h1Partida = 'Partida';
+const tituloLogin = h1Login + ' - ' + aplicacion;
+const tituloRegistro = h1Registro + ' - ' + aplicacion;
+const tituloPartida = h1Partida + ' - ' + aplicacion;
