da7278d8e354db52b5b44f66512cdabb889d7670
Quico Saval <quicosv@gmail.com>
2023-09-06 18:53:15 +0200

    Cambia los encabezados y los títulos de las páginas que funcionan actualmente

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { aplicacion } from '../variables';
 
 export const AuthLayout = () => {
   return (
     <>
+    <header>
+      <h1>{aplicacion}</h1>
+    </header>
       <main className="container">
         <Outlet />
       </main>
