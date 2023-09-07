ef4826cb68994079e1a4f8d5718867e580f7777c
quicosv <quicosv@gmail.com>
2023-09-07 17:44:18 +0200

    Hace que el foco caiga en el h1 cuando se cargue el componente correspondiente en los componentes que funcionan (#22)

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
 import { SignInForm } from './SignInForm';
 import { h1Login, tituloLogin } from '../../variables';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const LoginPage = () => {
   useEffect(() => {
     document.title = tituloLogin;
   }, []);
+  const h1Ref = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    if (h1Ref.current) {
+      h1Ref.current.focus();
+    }
+  },[]);
+
   return (
     <>
-      <h1>{h1Login}</h1>
+      <h1 ref={h1Ref}>{h1Login}</h1>
       <div className="row">
         <div className="col">
           <SignInForm />
--- frontend/src/pages/login/RegistroPage.tsx
+++ frontend/src/pages/login/RegistroPage.tsx
@@ -5,10 +5,16 @@ import { SignUpForm } from './SignUpForm';
 export const RegistroPage = () => {
   useEffect(() => {
     document.title = tituloRegistro;
-  })
+  });
+  const h1Ref = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    if (h1Ref.current) {
+      h1Ref.current.focus();
+    }
+  },[]);
   return (
     <>
-      <h1>{h1Registro}</h1>
+      <h1 ref={h1Ref} tabIndex={-1}>{h1Registro}</h1>
       <div className="row">
         
         <div className="col">
