f9905a542b792dce94bc827b7ec28715f24653e8
piscinagua <correo_javi@hotmail.com>
2023-09-08 18:14:15 +0200

    Cambia usuario en SignInForm.tsx.

--- frontend/src/pages/login/SignInForm.tsx
+++ frontend/src/pages/login/SignInForm.tsx
@@ -1,22 +1,23 @@
-import { FormEvent, useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useForm } from '../../hooks/useForm';
-import { ILoginResponse, ISignIn } from '../../interfaces/login.interface';
-import { ILocalStorageInfo } from '../../interfaces/localStorageInfo.interface';
-import { handlerAxiosError } from '../../helpers/handlerAxiosError';
-import { IJugadorInfoContext } from '../../interfaces/context.interface';
-import { AppContext } from '../../context/AppContext';
-import { clienteAxios } from '../../config/clienteAxios';
+import { FormEvent, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import { ILoginResponse, ISignIn } from "../../interfaces/login.interface";
+import { ILocalStorageInfo } from "../../interfaces/localStorageInfo.interface";
+import { handlerAxiosError } from "../../helpers/handlerAxiosError";
+import { IJugadorInfoContext } from "../../interfaces/context.interface";
+import { AppContext } from "../../context/AppContext";
+import { clienteAxios } from "../../config/clienteAxios";
 
 export const SignInForm = () => {
-	const { setJugadorInfo: setUsuarioInfo } = useContext<IJugadorInfoContext>(AppContext);
-	const [errorMsg, setErrorMsg] = useState<string>('');
+	const { setJugadorInfo: setJugadorInfo } =
+		useContext<IJugadorInfoContext>(AppContext);
+	const [errorMsg, setErrorMsg] = useState<string>("");
 	const [loading, setLoading] = useState<boolean>(false);
 	const navigate = useNavigate();
 
 	const { form, onInputChange } = useForm<ISignIn>({
-		email: '',
-		password: ''
+		email: "",
+		password: "",
 	});
 
 	const { email, password } = form;
@@ -26,17 +27,20 @@ export const SignInForm = () => {
 
 		try {
 			setLoading(true);
-			setErrorMsg('');
-			const { data } = await clienteAxios.post<ILoginResponse>('/auth/login', { email, password });
-			const infoUsuarioStorage: ILocalStorageInfo = {
+			setErrorMsg("");
+			const { data } = await clienteAxios.post<ILoginResponse>("/auth/login", {
+				email,
+				password,
+			});
+			const infoJugadorStorage: ILocalStorageInfo = {
 				email: data.email,
-				token: data.token
+				token: data.token,
 			};
-			localStorage.setItem('usuarioInfo', JSON.stringify(infoUsuarioStorage));
-			setUsuarioInfo({ email: data.email, socket: undefined });
+			localStorage.setItem("jugadorInfo", JSON.stringify(infoJugadorStorage));
+			setJugadorInfo({ email: data.email, socket: undefined });
 			setLoading(false);
-			navigate('/chat', {
-				replace: true
+			navigate("/chat", {
+				replace: true,
 			});
 		} catch (error) {
 			setLoading(false);
@@ -50,7 +54,15 @@ export const SignInForm = () => {
 			<form onSubmit={login}>
 				<div className="form-group">
 					<label htmlFor="email">Correo electrónico</label>
-					<input id="email" type="email" className="form-control" value={email} onChange={onInputChange} title='El correo que utilizó para registrarse.' required />
+					<input
+						id="email"
+						type="email"
+						className="form-control"
+						value={email}
+						onChange={onInputChange}
+						title="El correo que utilizó para registrarse."
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="password">Contraseña</label>
@@ -60,7 +72,7 @@ export const SignInForm = () => {
 						className="form-control"
 						value={password}
 						onChange={onInputChange}
-						title='El sistema distingue entre mayúsculas y minúsculas.'
+						title="El sistema distingue entre mayúsculas y minúsculas."
 						required
 					/>
 				</div>
