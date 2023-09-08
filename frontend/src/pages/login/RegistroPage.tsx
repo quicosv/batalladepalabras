5487bbb3cdbf4b660a256e236cb0ce7d9e919b86
Quico Saval <quicosv@gmail.com>
2023-09-08 18:49:17 +0200

    Actualiza una importación que faltaba

--- frontend/src/pages/login/RegistroPage.tsx
+++ frontend/src/pages/login/RegistroPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { h1Registro, tituloRegistro } from '../../variables';
 import { SignUpForm } from './SignUpForm';
 
