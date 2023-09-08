758d3c690bee8e0f76829f8f5ee831229acae24d
piscinagua <correo_javi@hotmail.com>
2023-09-08 17:48:22 +0200

    Cambia usuario en JugadoresConnectedList.

--- frontend/src/components/JugadoresConnectedList.tsx
+++ frontend/src/components/JugadoresConnectedList.tsx
@@ -4,16 +4,16 @@ import { AppContext } from '../context/AppContext';
 import { IJugadorInfoContext } from '../interfaces/context.interface';
 
 export const UsersConnectedList = () => {
-	const { jugadorInfo: usuarioInfo } = useContext<IJugadorInfoContext>(AppContext);
+	const { jugadorInfo: jugadorInfo } = useContext<IJugadorInfoContext>(AppContext);
 	const [jugadoresConectados, setJugadoresConectados] = useState<IJugadorConectado[]>([]);
-	const { socket } = usuarioInfo;
+	const { socket } = jugadorInfo;
 	socket?.on('jugadores-conectados', (jugadoresConectados: IJugadorConectado[]) => {
 		setJugadoresConectados(jugadoresConectados);
 	});
 
 	return (
 		<>
-			<h2>Usuarios conectados</h2>
+			<h2>Jugadores conectados</h2>
 			<ul className="list-group">
 				{jugadoresConectados.map((x) => (
 					<li className="list-group-item" key={x.idSesion}>
