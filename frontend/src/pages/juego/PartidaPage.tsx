0e22b0f9b84982146b4efd573bfed9a7028cc46b
Quico Saval <quicosv@gmail.com>
2023-09-13 17:55:08 +0200

    Hace que se borre la letra del input na vez que se comprueba todo

--- frontend/src/pages/juego/PartidaPage.tsx
+++ frontend/src/pages/juego/PartidaPage.tsx
@@ -12,32 +12,33 @@ export const PartidaPage = () => {
 		letra: ''
 	});
 
-const {letra} = form;
+	const { letra } = form;
 
 	const pruebaLetra = (e: FormEvent) => {
 		e.preventDefault();
 		letrasProbadas.push(letra);
-if (palabra.includes(letra)) {
-	letrasAcertadas.push(letra);
-}
-else {
-	setTuTurno(false);
-}
+		if (palabra.includes(letra)) {
+			letrasAcertadas.push(letra);
+		}
+		else {
+			setTuTurno(false);
+		}
+		onResetForm();
 	}
 	useEffect(() => {
 		document.title = tituloPartida;
 	})
 	return (
 		<>
-		<h1>{h1Partida}</h1>
-		{tuTurno ? (
-			<form onSubmit={pruebaLetra}>
-				<label htmlFor="letra">Letra</label>
-				<input type="text" maxLength={1} pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]{1}" id="letra" value={letra} onChange={onInputChange} required />
-				<button type="submit">Probar</button>
-			</form>
-		)
-		: (<p>El turno pasa al otro jugador.</p>)}
+			<h1>{h1Partida}</h1>
+			{tuTurno ? (
+				<form onSubmit={pruebaLetra}>
+					<label htmlFor="letra">Letra</label>
+					<input type="text" maxLength={1} pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]{1}" id="letra" value={letra} onChange={onInputChange} required />
+					<button type="submit">Probar</button>
+				</form>
+			)
+				: (<p>El turno pasa al otro jugador.</p>)}
 		</>
 	)
 }
