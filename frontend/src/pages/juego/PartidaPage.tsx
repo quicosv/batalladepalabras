b44e9990f1fb3a7d6b6938a54d346e2884da5b25
Quico Saval <quicosv@gmail.com>
2023-09-13 17:45:21 +0200

    Co-authored-by: Dinoscor software <dinoscor@users.noreply.github.com> Co-authored-by: MsKenmasters@hotmail.com <MsKenmasters@hotmail.com> Co-authored-by: juanluisochoa@hotmail.com <juanluisochoa@hotmail.com> añade una parte de la lógica para pasar el turno al oponente cuando se falle

--- frontend/src/pages/juego/PartidaPage.tsx
+++ frontend/src/pages/juego/PartidaPage.tsx
@@ -1,10 +1,29 @@
-import { useEffect, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import { h1Partida, tituloPartida } from "../../variables";
+import { useForm } from "../../hooks/useForm";
+import { ILetra } from "../../interfaces/letra.interface";
 
 export const PartidaPage = () => {
 	const [tuTurno, setTuTurno] = useState<boolean>(true);
 	const palabra: string = 'gato';
-	
+	const letrasProbadas: string[] = [];
+	const letrasAcertadas: string[] = [];
+	const { form, onInputChange, onResetForm } = useForm<ILetra>({
+		letra: ''
+	});
+
+const {letra} = form;
+
+	const pruebaLetra = (e: FormEvent) => {
+		e.preventDefault();
+		letrasProbadas.push(letra);
+if (palabra.includes(letra)) {
+	letrasAcertadas.push(letra);
+}
+else {
+	setTuTurno(false);
+}
+	}
 	useEffect(() => {
 		document.title = tituloPartida;
 	})
@@ -12,9 +31,9 @@ export const PartidaPage = () => {
 		<>
 		<h1>{h1Partida}</h1>
 		{tuTurno ? (
-			<form>
+			<form onSubmit={pruebaLetra}>
 				<label htmlFor="letra">Letra</label>
-				<input type="text" id="letra" value={letra} onChange={onInputChange} required />
+				<input type="text" maxLength={1} pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]{1}" id="letra" value={letra} onChange={onInputChange} required />
 				<button type="submit">Probar</button>
 			</form>
 		)
