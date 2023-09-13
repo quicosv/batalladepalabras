17cb252972d5089e429321690b82c382d7a7c863
Quico Saval <quicosv@gmail.com>
2023-09-13 19:26:57 +0200

    Muestra de forma accesible el estado de la palabra descubierta e informa al usuario de si ha ganado

--- frontend/src/pages/juego/PartidaPage.tsx
+++ frontend/src/pages/juego/PartidaPage.tsx
@@ -5,20 +5,35 @@ import { ILetra } from "../../interfaces/letra.interface";
 
 export const PartidaPage = () => {
 	const [tuTurno, setTuTurno] = useState<boolean>(true);
+	const [hasGanado, setHasGanado] = useState<boolean>(true);
 	const palabra: string = 'gato';
+	let descubierto: string = palabra.replace(/[a-zA-Z]/g, '_');
 	const letrasProbadas: string[] = [];
-	const letrasAcertadas: string[] = [];
 	const { form, onInputChange, onResetForm } = useForm<ILetra>({
 		letra: ''
 	});
 
 	const { letra } = form;
 
+const actualizarDescubierto = (letra: string): string => {
+	for (let i=0; i<palabra.length; i++){
+		if (letra === palabra[i]) {
+			descubierto = descubierto.substring(0,i) + letra + descubierto.substring(i+1);
+		}
+	}
+	return descubierto;
+}
+
+
+
 	const pruebaLetra = (e: FormEvent) => {
 		e.preventDefault();
 		letrasProbadas.push(letra);
 		if (palabra.includes(letra)) {
-			letrasAcertadas.push(letra);
+			actualizarDescubierto(letra);
+			if (descubierto === palabra) {
+				setHasGanado(true);
+			}
 		}
 		else {
 			setTuTurno(false);
@@ -32,11 +47,31 @@ export const PartidaPage = () => {
 		<>
 			<h1>{h1Partida}</h1>
 			{tuTurno ? (
+				<>
 				<form onSubmit={pruebaLetra}>
 					<label htmlFor="letra">Letra</label>
 					<input type="text" maxLength={1} pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]{1}" id="letra" value={letra} onChange={onInputChange} required />
 					<button type="submit">Probar</button>
 				</form>
+				<h2>Estado de la palabra</h2>
+				<ol>
+					{
+						[...descubierto].map(x=>(
+							<li>
+						{x === '_' ? 'Desconocido.' : x}
+						</li>
+						))
+					}
+					{/* {
+					for (let i = 0; i <= descubierto.length; i++) {
+						<li>
+						{descubierto[i] === '_' ? ('Desconocido.') : (descubierto[i])}
+						</li>
+					}
+					} */}
+				</ol>
+				{hasGanado && (<p>Has ganado.</p>)}
+</>
 			)
 				: (<p>El turno pasa al otro jugador.</p>)}
 		</>
