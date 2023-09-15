dc92022cd07e38f0b39f3731c437d91b04a93fec
Quico Saval <quicosv@gmail.com>
2023-09-15 16:49:44 +0200

    Añade la función que ayudará a procesar letras con acentos y diéresis

--- frontend/src/hooks/useLetra.ts
+++ frontend/src/hooks/useLetra.ts
@@ -1,1 +1,26 @@
-// export const
+export const letraSinAcentos = (letra: string): string => {
+	let letraResultante: string = '';
+	switch (letra.toLowerCase()) {
+		case 'á':
+			letraResultante = 'a';
+			break;
+		case 'é':
+			letraResultante = 'e';
+			break;
+		case 'í':
+			letraResultante = 'i';
+			break;
+		case 'ó':
+			letraResultante = 'o';
+			break;
+		case 'ú':
+			letraResultante = 'u';
+			break;
+		case 'ü':
+			letraResultante = 'u';
+			break;
+		default:
+			letraResultante = letra.toLowerCase();
+	}
+	return letraResultante;
+}
