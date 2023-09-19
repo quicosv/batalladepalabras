b18974cdbe573334c408817559e7dad18d1f26ad
piscinagua <correo_javi@hotmail.com>
2023-09-19 18:21:13 +0200

    Añade cambios para enviar la palabra  junto al email del jugador y el identificador de la partida a /palabra en el backend mediante Axios.

--- frontend/src/pages/juego/PalabraPage.tsx
+++ frontend/src/pages/juego/PalabraPage.tsx
@@ -1,30 +1,60 @@
-import { FormEvent, useEffect, useRef } from "react";
+import { FormEvent, useContext, useEffect, useRef, useState } from "react";
 import { h1Palabra, tituloPalabra } from "../../variables";
 import { useForm } from "../../hooks/useForm";
-import { IPalabra } from "../../interfaces/palabra.interface";
+import { AppContext } from "../../context/AppContext";
+import { IJugadorInfoContext } from "../../interfaces/context.interface";
+import { ITuPalabra } from "../../interfaces/tuPalabra.interface";
+import { clienteAxios } from "../../config/clienteAxios";
+import { handlerAxiosError } from "../../helpers/handlerAxiosError";
 
-export const PalabraPage = () => {
-	const { form, onInputChange, onResetForm } = useForm<IPalabra>({
-		palabra: ''
+interface IPalabraFormProps {
+	idPartida: number;
+	palabra: string;
+}
+
+export const PalabraPage = ({ idPartida, palabra }: IPalabraFormProps) => {
+	const { jugadorInfo } = useContext<IJugadorInfoContext>(AppContext);
+
+	const [errorMsg, setErrorMsg] = useState<string>("");
+	const [loading, setLoading] = useState<boolean>(false);
+
+	const { form, onInputChange, onResetForm } = useForm<ITuPalabra>({
+		jugadores_email: jugadorInfo.email,
+		tuPalabra: "",
+		partidas_idPartida: idPartida,
 	});
 
-	const { palabra } = form;
+	const { jugadores_email, tuPalabra, partidas_idPartida } = form;
 
-
-	const procesaPalabra = (e: FormEvent) => {
+	const procesaPalabra = async (e: FormEvent) => {
 		e.preventDefault();
+		try {
+			setLoading(true);
+			setErrorMsg("");
+			await clienteAxios.post<ITuPalabra>("/palabra", {
+				jugadores_email,
+				tuPalabra,
+				partidas_idPartida,
+			});
+			setLoading(false);
+		} catch (error) {
+			setLoading(false);
+			const errores = await handlerAxiosError(error);
+			setErrorMsg(errores);
+		}
+
 		onResetForm();
-	}
+	};
 
 	useEffect(() => {
 		document.title = tituloPalabra;
-	},[]);
+	}, []);
 	const inputRef = useRef<HTMLInputElement>(null);
 	useEffect(() => {
 		if (inputRef.current) {
 			inputRef.current.focus();
 		}
-	},[]);
+	}, []);
 	return (
 		<>
 			<h1>{h1Palabra}</h1>
@@ -35,13 +65,24 @@ export const PalabraPage = () => {
 					maxLength={23}
 					pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]{1,23}"
 					id="palabra"
-					value={palabra}
+					value={tuPalabra}
 					onChange={onInputChange}
 					ref={inputRef}
 					required
 				/>
 				<button type="submit">Enviar</button>
 			</form>
+			{loading && (
+				<div className="alert alert-warning" role="alert">
+					Enviando palabra...
+				</div>
+			)}
+			{/* Si errorFetch es true, mostramos un mensaje de error al usuario */}
+			{errorMsg && !loading && (
+				<div className="alert alert-danger" role="alert">
+					{errorMsg}
+				</div>
+			)}
 		</>
 	);
 };
