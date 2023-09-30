df8811203079849e1779b72d35c3459f725241ba
Quico Saval <quicosv@gmail.com>
2023-09-30 09:55:23 +0200

    Elimina los helpers que no sirven

--- backend/helpers/dbValidators.ts
+++ backend/helpers/dbValidators.ts
@@ -1,4 +1,3 @@
-import { Partida } from "../models/partida";
 import { Jugador } from "../models/jugador";
 
 export const emailExiste = async (email = "") => {
@@ -10,27 +9,5 @@ export const emailExiste = async (email = "") => {
 
 	if (existeEmail) {
 		throw new Error(`El email: ${email}, ya está registrado`);
-	}
-};
-
-export const partidaExiste = async (nombre = "") => {
-	const existePartida = await Partida.findOne({
-		where: {
-			nombre: nombre,
-		},
-	});
-
-	if (existePartida) {
-		throw new Error(
-			`No se puede crear. La partida ${nombre} ya existe. Elija otro diferente`
-		);
-	}
-};
-
-export const numeroLetrasValidoPartida = async (numeroLetras: number) => {
-	if (numeroLetras < 1 || numeroLetras > 23) {
-		throw new Error(
-			`No se puede crear la partida. El número de letras (${numeroLetras}) debe estar entre 1 y 23.`
-		);
 	}
 };
